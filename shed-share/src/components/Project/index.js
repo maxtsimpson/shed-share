cc3301159302118ee365324946544fcb5bc08695
Max Simpson <maxsimpson95@gmail.com>
2020-07-20 20:40:29 +0800

    starting to get project timeline to be dynamic

--- shed-share/src/components/Project/index.js
+++ shed-share/src/components/Project/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import M, { Navbar, Icon, NavItem } from 'react-materialize'
 
-function Project() {
+function Project({projectTitle, projectSteps=[], projectMaterialsList, projectImage}) {
     return (
-        <div></div>
+        <h1>{projectTitle}</h1>
+        {projectSteps.map(step => {
+            
+        })}
     )
 }
 
--- shed-share/src/components/ProjectTimeLine/index.js
+++ shed-share/src/components/ProjectTimeLine/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Col, Card, CardTitle, Icon } from 'react-materialize'
-import {Timeline, TimelineEvent} from 'react-event-timeline'
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+import { Timeline, TimelineEvent } from 'react-event-timeline'
+import MaterialIcon, { colorPalette } from 'material-icons-react';
 import './style.css'
 
 const timeLineColor = '#212121'
@@ -9,17 +9,17 @@ const timeLineColor = '#212121'
 function ProjectTimeline() {
     return (
         <>
-        <Timeline>
+            <Timeline>
                 <TimelineEvent title="Research"
-                            createdAt="2020-07-12 10:06 PM"
-                            icon={<MaterialIcon icon="biotech" color={timeLineColor}></MaterialIcon>}
+                    createdAt="2020-07-12 10:06 PM"
+                    icon={<MaterialIcon icon="biotech" color={timeLineColor}></MaterialIcon>}
                 >
                     I've had a look at what tools i need for the project. whether i could use angle grinder or an oxy torch
                 </TimelineEvent>
                 <TimelineEvent title="Design"
-                            createdAt="2020-07-14 10:06 PM"
-                            icon={<MaterialIcon icon="design_services" color={timeLineColor}></MaterialIcon>}
-                            
+                    createdAt="2020-07-14 10:06 PM"
+                    icon={<MaterialIcon icon="design_services" color={timeLineColor}></MaterialIcon>}
+
                 >
                     I've sketched out a diagram with some measurements. used the kitchen bench height as a guide for my bench height
                 </TimelineEvent>
@@ -30,8 +30,20 @@ function ProjectTimeline() {
                 >
                     Cut some old patio steel to length and started on some welds.
                 </TimelineEvent>
-        </Timeline>
+            </Timeline>
         </>
+    )
+
+    return (
+        <Timeline>
+            {steps.map((step, index) => {
+                <TimelineEvent title={stepCategory.Title}
+                    createdAt={stepCreatedAt}
+                    icon={<MaterialIcon icon={stepCategory.Icon} color={timeLineColor}></MaterialIcon>}
+                >
+                </TimelineEvent>
+            })}
+        </Timeline>
     )
 }
 
