88628369eb34efeae1ec62a3dd0d61efd6d10fee
Max Simpson <maxsimpson95@gmail.com>
2020-07-21 14:41:38 +0800

    edited seeders index to run each seed file against db

--- seeders/index.js
+++ seeders/index.js
@@ -1,17 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const connection = require("./config/mongo.js")
 const basename = path.basename(__filename);
+
 const seedFunctions = {}
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        seedFunctions[file] = require(path.join(__dirname,file))
-        console.log(`====== running ${file} seed ========`)
-        seedFunctions[file]()
+connection.then(() => {
+    fs
+        .readdirSync(__dirname)
+        .filter(file => {
+            return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+        })
+        .forEach(async file => {
+            seedFunctions[file] = require(path.join(__dirname, file))
+            console.log(`====== running ${file} seed ========`)
+            await seedFunctions[file]()
+        })
+        .then(() => {
+            console.log("***** completed seed operation ******")
+            process.exit(0);
+        });
+})
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
     });
 
 module.exports = seedFunctions;
