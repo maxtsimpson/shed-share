1be23b01602a5b84a270ee9c3f9333894000a118
Max Simpson <maxsimpson95@gmail.com>
2020-07-21 14:42:59 +0800

    modified users seed to export seedUsers function

--- seeders/users.js
+++ seeders/users.js
@@ -29,17 +29,4 @@ const seedUsers = async (numberOfUsers = 20) => {
     }
 }
 
-const generateUsers = (numberOfUsers = 20) => {
-    const userList = []
-    for (let index = 0; index < numberOfUsers; index++) {
-        const user = {
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            picture: faker.image.avatar()
-        }
-        userList.push(user)
-    }
-    return userList
-}
-
-export default generateUsers
+export default seedUsers
