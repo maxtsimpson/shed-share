d6518b06b9cf84d4a02471f59276ef61da7cc884
Max Simpson <maxsimpson95@gmail.com>
2020-07-23 05:58:15 +0800

    simplified user seed

--- seeders/index.js
+++ seeders/index.js
@@ -2,28 +2,40 @@ const fs = require('fs');
 const path = require('path');
 const connection = require("../config/mongo")
 const basename = path.basename(__filename);
+const users = require("./users")
 
 const seedFunctions = {}
 
+// connection.then(() => {
+//     fs
+//         .readdirSync(__dirname)
+//         .filter(file => {
+//             return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//         })
+//         .forEach(async file => {
+//             seedFunctions[file] = require(path.join(__dirname, file))
+//             console.log(`====== running ${file} seed ========`)
+//             await seedFunctions[file]()
+//         })
+//         // .then(() => {
+//             console.log("***** completed seed operation ******")
+//             process.exit(0);
+//         // });
+// })
+//     .catch(err => {
+//         console.error(err);
+//         process.exit(1);
+//     });
+
 connection.then(() => {
-    fs
-        .readdirSync(__dirname)
-        .filter(file => {
-            return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-        })
-        .forEach(async file => {
-            seedFunctions[file] = require(path.join(__dirname, file))
-            console.log(`====== running ${file} seed ========`)
-            await seedFunctions[file]()
-        })
-        // .then(() => {
-            console.log("***** completed seed operation ******")
-            process.exit(0);
-        // });
+    users()
+    .then(() => {
+        process.exit(0);
+    })
 })
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+.catch(err => {
+    console.error(err);
+    process.exit(1);
+});
 
 module.exports = seedFunctions;
