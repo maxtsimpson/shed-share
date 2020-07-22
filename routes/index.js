cd1b9f06fe5bf4b9a16dae1d6f8dd8bb73a22b08
Max Simpson <maxsimpson95@gmail.com>
2020-07-22 14:46:39 +0800

    edited routes index to load authRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const route = require("../")
+const authRoutes = require("./authRoutes");
+
 // API Routes
+router.use("/api", apiRoutes);
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
